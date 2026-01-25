1bae6a55ce23fcd30847f254a149b5aa14162eac
hyobin <hyobiny0328@gamil.com>
2026-01-25 20:36:48 +0900

    refactor: 컴포넌트 폴더 구조변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 
-import Header from './components/Header'
+import Header from './components/layout/Header'
 import './globals.css'
 
 const pretendard = localFont({
